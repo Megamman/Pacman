03cc02760e38321f50e95d0122f43d3bb69de38a
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-11 17:02:58 +0200

    ghost move ment update
    with phase one

---
+++ MyUnityProject/Assets/Scripts/GhostIntro.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostIntro : MonoBehaviour {
+	public Transform[] introWaypoints;
+	public Transform[] startWaypoints;
+	int cur = 0;
+	int phase = 0;
+
+	public float speed = 0.3f;
+
+	void Start()
+	{
+		Invoke ("ToPhaseOne", 2f);
+	}
+
+	void FixedUpdate () {
+		if (phase == 0)
+		{
+			if (transform.position.Equals (introWaypoints [cur].position))
+			{
+				// if condition on one line - switch between 1 and 0.
+				cur = (cur == 0) ? 1 : 0;
+			}
+			transform.position = Vector2.MoveTowards (transform.position, introWaypoints [cur].position, speed);
+		}
+
+		if (phase == 1)
+		{
+			if (transform.position.Equals (startWaypoints [cur].position)) {
+				cur++;
+				if (cur == 2) {
+					this.enabled = false;
+				}
+			} else {
+				transform.position = Vector2.MoveTowards (transform.position, startWaypoints [cur].position, speed);
+			}
+		}
+	}
+
+	void ToPhaseOne()
+	{
+		phase = 1;
+		cur = 0;
+	}
+}
---
+++ MyUnityProject/Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostMovement : MonoBehaviour {
+
+	public float speed = 0.4f;
+	Vector2 dest = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		dest = transform.position;
+		Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
+		transform.Rotate(randomDirection);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Move closer to Destination
+		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+		GetComponent<Rigidbody2D>().MovePosition(p);
+
+		// Animation Parameters
+		Vector2 dir = dest - (Vector2)transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+
+	}
+
+	bool valid(Vector2 dir) {
+		// Cast Line from 'next to Pac-Man' to 'Pac-Man'
+		Vector2 pos = transform.position;
+		RaycastHit2D hit = Physics2D.Linecast(pos, pos + dir);
+		return (hit.collider == GetComponent<Collider2D>());
+	}
+
+
+}
+
