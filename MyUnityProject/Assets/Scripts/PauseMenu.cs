879d560e5df5a54c92c935df1cbaa0b96470525b
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-12 12:41:25 +0200

    Now with a Pause Menu
    addit a working pause menu
    press `esc to go to main menu
    press space to resume

---
+++ MyUnityProject/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject Game;
+    public GameObject Mainmenu;
+    public GameObject Pausemenu;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Game.SetActive(false);
+                Mainmenu.SetActive(true);
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    void Resume()
+    {
+        Pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
